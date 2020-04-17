24d5912993e2251035ee2a36a5d993aea79e2af5
Kurt Seifried <kseifried@cloudsecurityalliance.org>
2020-04-17 14:40:52 -0600

    utility stub files

---
+++ utilities/compare-Glossary-master-list.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+# Compares the Glossary-master-list.txt in the local directory against ARGV0 (a text file) and does a fuzzy-wuzzy comparision and generates a report showing the highest match for each word in the new text file
---
+++ utilities/generate-Glossary-master-list.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+# Generates the Glossary-master-list.txt in the local directory, assumes it is run from Glossary git repo root directory (./glossary/*/*.md basically)
+
