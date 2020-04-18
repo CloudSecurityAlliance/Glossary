5a4b3504d0035b27c895b2651a2d0497b9cb5d29
Kurt Seifried <kseifried@cloudsecurityalliance.org>
2020-04-17 19:24:20 -0600

    Glossary-master-list.txt creation script

--- utilities/generate-Glossary-master-list.py
+++ utilities/generate-Glossary-master-list.py
@@ -2,3 +2,13 @@
 
 # Generates the Glossary-master-list.txt in the local directory, assumes it is run from Glossary git repo root directory (./glossary/*/*.md basically)
 
+import os
+
+root_dir = "glossary"
+
+fileoutput = "Glossary-master-list.txt"
+
+with open(fileoutput, 'w') as results:
+    for dir_, _, files in os.walk(root_dir):
+        for file_name in files:
+            results.write(file_name + "\n")
