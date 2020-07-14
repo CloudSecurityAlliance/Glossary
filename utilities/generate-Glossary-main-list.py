3883770a3ca049d4c284caf070d9353a361873cf
Kurt Seifried <kurt@seifried.org>
2020-07-14 08:30:45 -0600

    Create generate-Glossary-main-list.py
    Updated terminology

---
+++ utilities/generate-Glossary-main-list.py
@@ -0,0 +1,14 @@
+\#!/usr/bin/python
+
+# Generates the Glossary-main-list.txt in the local directory, assumes it is run from Glossary git repo root directory (./glossary/*/*.md basically)
+
+import os
+
+root_dir = "glossary"
+
+fileoutput = "Glossary-master-list.txt"
+
+with open(fileoutput, 'w') as results:
+    for dir_, _, files in os.walk(root_dir):
+        for file_name in files:
+            results.write(file_name + "\n")
